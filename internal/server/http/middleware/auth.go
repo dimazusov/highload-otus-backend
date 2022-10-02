52dc49bec0add45d469abd3300bee82abf31efbd
usov <dmitrii.usov@sbermegamarket.ru>
2022-10-02 19:19:54 +0300

    update

--- internal/server/http/middleware/auth.go
+++ internal/server/http/middleware/auth.go
@@ -39,6 +39,7 @@ func Cors(app *app.App) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Content-Type", "application/json")
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "51.250.66.160")
 		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "X-Auth-Token, Content-Type, Content-Length, Accept-Encoding")
